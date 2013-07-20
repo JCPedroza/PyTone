3f5b89ca0a07cb84b85011544d1ae58d64269884
JCPedroza <nemesis4go10@hotmail.com>
2013-07-20 06:11:03 -0600

    added __str__

--- Note.py
+++ Note.py
@@ -2,6 +2,15 @@ from Sound import Sound
 
 
 class Note(Sound):
-    def __init__(self, frequency, velocity=64, attack=0, decay=0, sustain=127, release=0, name="none"):
+    def __init__(self, frequency, velocity=64, attack=0, decay=0, sustain=127,
+                 release=0, name="none"):
+
         Sound.__init__(self, frequency, velocity, attack, decay, sustain, release)
         self.name = name
+
+    def __str__(self):
+        """ called when printing a Note object, returns object's state """
+        return (" Name:" + self.name + " Freq:" + str(self.frequency) +
+                " Vel:" + str(self.velocity) + " Att:" + str(self.attack) +
+                " Dec:" + str(self.decay) + " Sus:" + str(self.sustain) +
+                " Rel:" + str(self.release))
--- Sound.py
+++ Sound.py
@@ -6,3 +6,9 @@ class Sound:
         self.decay = decay
         self.sustain = sustain
         self.release = release
+
+    def __str__(self):
+        """ called when printing a Sound object, returns object's state """
+        return ("Freq:" + str(self.frequency) + " Vel:" + str(self.velocity) +
+                " Att:" + str(self.attack) + " Dec:" + str(self.decay) +
+                " Sus:" + str(self.sustain) + " Rel:" + str(self.release))
