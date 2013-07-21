de4df4c05096da68114891bef13549bf67dbbdab
JCPedroza <nemesis4go10@hotmail.com>
2013-07-21 09:03:15 -0600

    first commit chord.py

--- chord.py
+++ chord.py
@@ -1,3 +1,8 @@
 class Chord:
     def __init__(self, *notes):
         self.notes = notes
+
+    def print_notes(self):
+        """ print notes and their state """
+        for e in self.notes:
+            print e
