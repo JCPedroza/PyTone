738813903351883ba5d42389e9ad85c3fffc4313
JCPedroza <nemesis4go10@hotmail.com>
2013-07-21 09:09:20 -0600

    cleaned

--- main.py
+++ main.py
@@ -1,43 +1,10 @@
 # -*- coding: utf-8 -*-
-# Start OOP: start with a formulas object! Instantiate it here (more formulas will be added, that will become big, soon!!!
-# TO DO: scale_corrector(), harmonize() --needs to harmonize in other modes!-- , make comparisons between all possible notes in X parallel
-# harmonies and all existing chords in a scenario (diatonic scenario, or diatonic 7ths, for example)
-# apply systematic design
-# !!!!!!! ANTES DE QUE CONTINUES CAMBIA TODO A OBJETOS O SERA MAS DIFICIL HACERLO MAS TARDE !!!
 
 import formulas
 import tests
 from process import Process
 from note import Note
 from chord import Chord
-
-# --------------------------------------
-# -------- DATA DEFINITIONS ------------
-# --------------------------------------
-
-# note is : string
-# interp: the name of a note
-
-# chordname is: string
-# interp: the name of a chord
-
-# scale is: a list of: note
-# interp: the notes that form a scale
-
-# chord is: a list of: note
-# interp: the notes that form a chord
-
-# pool is: a list of note
-# interp: all the possible note of an octave, in ascending order
-
-# formula is: a list of int
-# interp: the steps that define a mode (semitones, half-steps, etc)
-
-# chordscale is: a list of: chord
-# interp: chords that form a harmony
-
-# harmony is: list of: chordname
-# interp: chordnames of chords that form a harmony
 
 # --------------------------------------
 # ------------ Program -----------------
